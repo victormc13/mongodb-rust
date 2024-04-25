1336be4250abcf7fad174c44beda52d15fde5165
victormc13 <viktorsb14@gmail.com>
2024-04-25 13:19:40 -0500

    new: Adds find_one query example

---
+++ rust_quickstart/src/main.rs
@@ -0,0 +1,31 @@
+use mongodb::{
+        bson::{Document, doc},
+        error::Error,
+        Client,
+        Collection
+};
+use dotenv::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+
+    // Load .env file where is environment variable
+    dotenv().ok();
+    // Get the env variable from .env file
+    let mongodb_uri = env::var("MONGODB_URI").expect("MONGODB_URI env variable not found");
+
+    // Create a new client and connect to the server
+    let client = Client::with_uri_str(mongodb_uri).await?;
+
+    // Get a handle on the database
+    let database = client.database("swapper_db");
+    let my_coll: Collection<Document> = database.collection("users");
+
+    // Find a movie based on the title value
+    let my_user = my_coll.find_one(doc! {"email": "leonscott@gmail.com"}, None).await?;
+
+    // Print the document
+    println!("Found a user: \n{:#?}", my_user.unwrap());
+    Ok(())
+}
